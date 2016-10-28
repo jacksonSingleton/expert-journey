42209625a3a54f7a3b955036c948b27d65ef22d1
Jackson Singleton <jsingleton12321@yahoo.com>
2016-10-28 04:55:09 -0500

    First update, added Boostrap and working on the greeter

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,56 @@
 
-var Greeter = React.createClass({
+var GreeterMethod = React.createClass({
+
   render: function() {
     return (
       <div>
-        <h1>Hello, React</h1>
+        <h1>Some h1</h1>
+        <p>Some paragraph</p>
+      </div>
+    );
+  }
+
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return{
+      name: "React",
+      message: "Default Message!"
+    }
+  },
+  getInitialState: function() {
+    return{
+      name: this.props.name
+    }
+  },
+
+  onButtonClick: function(e){
+    e.preventDefault();
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+    if (typeof name == 'string' && name.length > 0){
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function() {
+
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello, {name}!</h1>
+        <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" placeholder="Your name here"/>
+          <br />
+          <br />
+          <button className="btn">Set Name!</button>
+        </form>
       </div>
     );
   }
@@ -11,6 +58,6 @@ var Greeter = React.createClass({
 });
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name="Jackson" message="Welcome!"/>,
   document.getElementById('app')
 );
